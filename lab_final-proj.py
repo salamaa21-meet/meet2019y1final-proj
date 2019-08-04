3c34b5d794a919fe49ee13dea8770f75d96bbefb
salamaa21-meet <salamaa21@meet.mit.edu>
2019-08-04 10:56:54 -0400

    Stopping here

--- lab_final-proj.py
+++ lab_final-proj.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 turtle.tracer(1,0)
 
 UP_EDGE = 400
@@ -6,8 +7,11 @@ DOWN_EDGE = -500
 RIGHT_EDGE = 460
 LEFT_EDGE = -460
 
-screen=turtle.Screen()
-screen.bgpic("source3.gif")
+screen=turtle.clone()
+turtle.register_shape("bgpic.gif")
+screen.shape("bgpic.gif")
+screen.shapesize(1000,900)
+
 #Window size and screen
 
 #Box is the reqtangle where we see the riddles
@@ -16,26 +20,14 @@ box.color('red', 'lightgreen')
 box.begin_fill()
 box.hideturtle()
 box.penup()
-box.goto(460,400)
+box.goto(460,500)
 box.pendown()
-box.goto(460,200)
-box.goto(-460,200)
-box.goto(-460,400)
-box.goto(460,400)
+box.goto(460,300)
+box.goto(-460,300)
+box.goto(-460,500)
+box.goto(460,500)
 box.end_fill()
 #The question's box is ready
 
-
-#the grass 
-turtle.register_shape("Grass.gif")
-
-grass=turtle.clone()
-grass.penup()
-grass.shape("Grass.gif")
-grass.goto(250,-350)
-
-grass2=turtle.clone()
-grass2.penup()
-grass2.shape("Grass.gif")
-grass2.goto(-250,-350)
-
+turtle.register_shape('tree1.gif')
+turtle.register_shape('tree2.gif')
