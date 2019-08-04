feccae6ed43860eedacd15b672e993e31c461990
salamaa21-meet <salamaa21@meet.mit.edu>
2019-08-04 09:21:03 -0400

    Stopping here

--- lab_final-proj.py
+++ lab_final-proj.py
@@ -5,7 +5,10 @@ UP_EDGE = 400
 DOWN_EDGE = -500
 RIGHT_EDGE = 460
 LEFT_EDGE = -460
-#Window size
+
+screen=turtle.Screen()
+screen.bgpic("source3.gif")
+#Window size and screen
 
 #Box is the reqtangle where we see the riddles
 box=turtle.clone()
@@ -22,4 +25,17 @@ box.goto(460,400)
 box.end_fill()
 #The question's box is ready
 
+
+#the grass 
+turtle.register_shape("Grass.gif")
+
+grass=turtle.clone()
+grass.penup()
+grass.shape("Grass.gif")
+grass.goto(250,-350)
+
+grass2=turtle.clone()
+grass2.penup()
+grass2.shape("Grass.gif")
+grass2.goto(-250,-350)
 
